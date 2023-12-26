c09e5edf3d2ebfaaa420c60f787fce0be827871d
glay415 <ljs0912@midasit.com>
2023-12-26 17:23:39 +0900

    version up

--- mod.ts
+++ mod.ts
@@ -1,3 +1,3 @@
 export * from "./zod-validator.ts";
-export { initSwagger } from "./swagger-initializer.ts";
+export { initSwagger, transplantSwagger } from "./swagger-initializer.ts";
 export { z } from "./swagger-utils.ts"
--- swagger-initializer.ts
+++ swagger-initializer.ts
@@ -1,5 +1,7 @@
 import { generateRegister } from "./swagger-generator.ts";
 import { writeDocumentation } from "./swagger-utils.ts";
+import { Router } from 'oak/mod.ts'
+import { parse } from "yaml/es2022/yaml.mjs";
 
 export async function initSwagger(serverUrl: string, routerUrl: string, schemaPath: string, writePath: string, deniedMiddlewares: string[]) {    
     if (schemaPath.startsWith('.')) {
@@ -8,57 +10,40 @@ export async function initSwagger(serverUrl: string, routerUrl: string, schemaPa
     
     await generateRegister(routerUrl, schemaPath, deniedMiddlewares);
     writeDocumentation(writePath, serverUrl);
+}
 
-    const swaggerUIContent = 
-    `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="SwaggerUI" />
-        <title>Moa Catwagger</title>
-        <link href="data:image/x-icon;base64,AAABAAEAEBAQAAAAAAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAAjtv6ALSU/wD///8ADQ0NALhQzACMjIsA2X/rAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEZmQEZmQABERERERERAABFGZmZmZmQAdGZkREREZkBGImRmRmRiJEYiZmZmZmIkRmZEZmRkRmRGZjRmZmNGZEZmZmZmZmZkVGZmZmZmZkB0ZmZmZmZmQHRmZkREZmZAdGZkd1FGZkB0ZkdXcURmQFdEd3dxQEQAd3dXdxFAAADBBwAAAAcAAAADAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAEAAAABAAAAAQAAAAEAAAATAAAAHwAA" rel="icon" type="image/x-icon">
-        <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui.css" />
-    </head>
-    <body>
-        <div id="swagger-ui"></div>
-        <script src="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui-bundle.js" crossorigin></script>
-        <script src="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui-standalone-preset.js" crossorigin></script>
-        <script>
-            window.onload = () => {
-                window.ui = SwaggerUIBundle({
-                    url: "${writePath}" + "/openapi-docs.yml",
-                    dom_id: "#swagger-ui",
-                    presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
-                    layout: "StandaloneLayout"
-                });
-            };
-        </script>
-    </body>
-    </html>
-    `;
-    
-    const swaggerAppContent = 
-    `import { Application } from "https://deno.land/x/oak@v12.6.1/mod.ts";
-    import { send } from "https://deno.land/x/oak@v12.6.1/send.ts";
-    
-    const swaggerApp = new Application();
-    swaggerApp.use(async (ctx) => {
-        await send(ctx, ctx.request.url.pathname, {
-            root: Deno.cwd(),
-            index: "${writePath}" + "/swagger-ui.html",
-        });
-    })
-    
-    console.log(\`Swagger is listening on port 3000\`);
-    swaggerApp.listen({ port: 3000});
-    `;
-
-    const textEncorder = new TextEncoder();
-    const SwaggerUI = textEncorder.encode(swaggerUIContent);
-    Deno.writeFileSync(`${writePath}/swagger-ui.html`, SwaggerUI)
-
-    
-    const SwaggerApp = textEncorder.encode(swaggerAppContent);
-    Deno.writeFileSync(`${writePath}/swagger-app.ts`, SwaggerApp)
+export function transplantSwagger(apiSpecPath: string, router: Router) {
+    router.get(
+        '/swagger', 
+        (ctx) => {
+          const apiSpec = JSON.stringify(parse(Deno.readTextFileSync(Deno.cwd() + apiSpecPath)))
+          const ui = `<!DOCTYPE html>
+          <html lang="en">
+          <head>
+              <meta charset="utf-8" />
+              <meta name="viewport" content="width=device-width, initial-scale=1" />
+              <meta name="description" content="SwaggerUI" />
+              <title>Moa Catwagger</title>
+              <link href="data:image/x-icon;base64,AAABAAEAEBAQAAAAAAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAAjtv6ALSU/wD///8ADQ0NALhQzACMjIsA2X/rAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEZmQEZmQABERERERERAABFGZmZmZmQAdGZkREREZkBGImRmRmRiJEYiZmZmZmIkRmZEZmRkRmRGZjRmZmNGZEZmZmZmZmZkVGZmZmZmZkB0ZmZmZmZmQHRmZkREZmZAdGZkd1FGZkB0ZkdXcURmQFdEd3dxQEQAd3dXdxFAAADBBwAAAAcAAAADAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAEAAAABAAAAAQAAAAEAAAATAAAAHwAA" rel="icon" type="image/x-icon">
+              <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui.css" />
+          </head>
+          <body>
+              <div id="swagger-ui"></div>
+              <script src="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui-bundle.js" crossorigin></script>
+              <script src="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui-standalone-preset.js" crossorigin></script>
+              <script>
+                  const openApiSpec = ${apiSpec}
+                  window.onload = () => {
+                      window.ui = SwaggerUIBundle({
+                          spec: openApiSpec,
+                          dom_id: "#swagger-ui",
+                          presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+                          layout: "StandaloneLayout"
+                      });
+                  };
+              </script>
+          </body>
+          </html>
+          `
+          ctx.response.body = ui})
 }
