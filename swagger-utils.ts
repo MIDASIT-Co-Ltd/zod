d117c7994b19a3460f55425c103443e3af4dda69
glay415 <ljs0912@midasit.com>
2024-01-08 13:18:20 +0900

    mediatype all

--- swagger-utils.ts
+++ swagger-utils.ts
@@ -98,7 +98,7 @@ export function registerEndpoint(
         };
         if (details.schema) {
             responses[statusCode].content = {
-                'application/json': {
+                '*/*': {
                     schema: details.schema
                 }
             };
