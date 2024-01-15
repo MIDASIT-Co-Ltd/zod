21a49a239a2e3f2c124fc864d6dc635785ee828c
glay415 <ljs0912@midasit.com>
2024-01-15 14:44:15 +0900

    test

--- mod.ts
+++ mod.ts
@@ -1,3 +1,4 @@
 export * from "./zod-validator.ts";
+export * from "./zod-validator2.ts";
 export { initSwagger, transplantSwagger } from "./swagger-initializer.ts";
 export { z } from "./swagger-utils.ts"
