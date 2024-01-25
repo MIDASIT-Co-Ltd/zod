70cb35a74f8a68ddfa5a64a8feb38413ee47fcb4
glay415 <ljs0912@midasit.com>
2024-01-25 16:14:36 +0900

    fix: humman error

--- mod.ts
+++ mod.ts
@@ -1,3 +1,3 @@
 export * from "./zod-validator.ts";
-export { initSwagger, transplantSwagger } from "./swagger-initializer.ts";
+export * from "./swagger-initializer.ts";
 export { z } from "./swagger-utils.ts"
